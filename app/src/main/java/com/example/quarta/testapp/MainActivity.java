f1bcfd2054f62acb445c29ed30ccc62d363090ec
LeaX-XIV <leax_xiv@me.com>
2015-01-28 14:51:19 +0100

    first commit

---
+++ app/src/main/java/com/example/quarta/testapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.quarta.testapp;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.*;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(this, "Penis", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void encript(View view){
+        InputMethodManager inputManager =
+                (InputMethodManager) this.
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(
+                this.getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+
+        int p = Integer.parseInt(((EditText) findViewById(R.id.p)).getText().toString());
+        int q = Integer.parseInt(((EditText) findViewById(R.id.q)).getText().toString());
+        int m = Integer.parseInt(((EditText) findViewById(R.id.message)).getText().toString());
+
+        int f = (p-1)*(q-1);
+
+        for(int i = f - 1; i > 0; i--) {
+            
+        }
+    }
+}
