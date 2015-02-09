ca4e7d44adce13e9e72812733c9d694b84e2aba4
Alessandro Borione <leax_xiv@me.com>
2015-02-09 21:20:03 +0100

    Auto-Matic
    No button needed to start spinnin'.

--- app/src/main/java/com/example/quarta/testapp/EasterEgg.java
+++ app/src/main/java/com/example/quarta/testapp/EasterEgg.java
@@ -13,33 +13,30 @@ import android.widget.ImageView;
 
 public class EasterEgg extends ActionBarActivity {
 
-    MediaPlayer mPlayer;
-    ImageView imageView;
+    private MediaPlayer mPlayer;
+    private ImageView imageView;
+    private Animation animRotate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easter_egg);
         imageView = (ImageView) findViewById(R.id.animatedImage);
-        final Animation animRotate = AnimationUtils.loadAnimation(this, R.anim.rotate);
         mPlayer = MediaPlayer.create(EasterEgg.this, R.raw.song);
-        Button btnRotate = (Button) findViewById(R.id.rotate);
+        animRotate = AnimationUtils.loadAnimation(this, R.anim.rotate);
 
-        btnRotate.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                mPlayer.start();
-                imageView.startAnimation(animRotate);
-
-            }
-        });
+        doRotate();
     }
 
+    @Override
     public void onDestroy() {
         mPlayer.stop();
         super.onDestroy();
 
+    }
+
+    public void doRotate() {
+        mPlayer.start();
+        imageView.startAnimation(animRotate);
     }
 }
